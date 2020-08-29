2746473a5a7bb81df1c2173dfe7dc50737e1dec6
avaldevelopercontrol <70033727+avaldevelopercontrol@users.noreply.github.com>
2020-08-29 16:57:54 -0500

    Revisión 1.0
    Versión de script PowerShell de inventario de Hardware de pc's

---
+++ Get-Aval-Inventory.ps1
@@ -0,0 +1,15 @@
+﻿<#
+wmic diskdrive get  | Export-CSV $Ruta -append -force -NoTypeInformation
+
+Get-ComputerInfo | Export-Csv $Ruta
+#>
+Param( 
+    [string]$Ruta
+) 
+
+gwmi Win32_ComputerSystem | Export-CSV $Ruta -append -force -NoTypeInformation
+gwmi Win32_OperatingSystem | Export-CSV $Ruta -append -force -NoTypeInformation
+gwmi Win32_BIOS | Export-CSV $Ruta -append -force -NoTypeInformation
+gwmi Win32_Processor | Export-CSV $Ruta -append -force -NoTypeInformation
+gwmi Win32_NetworkAdapterConfiguration |` where{$_.IPEnabled -eq "True"} | Export-CSV $Ruta -append -force -NoTypeInformation
+gwmi Win32_DiskDrive | Export-CSV $Ruta -append -force -NoTypeInformation
---
+++ Inventario.bat
@@ -0,0 +1,4 @@
+
+powershell.exe -ExecutionPolicy Bypass -File "Get-Aval-Inventory.ps1" -Ruta %USERPROFILE%\desktop\inventory.csv
+
+pause
